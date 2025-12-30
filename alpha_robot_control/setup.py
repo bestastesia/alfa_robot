01f2debd96cb84e314746bcaa9ceba8c0181aa85
kkozia <16506744+kkozia@user.noreply.gitee.com>
2025-12-30 19:59:43 +0800

    Initial commit

---
+++ alpha_robot_control/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'alpha_robot_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kkozia',
+    maintainer_email='16506744+kkozia@user.noreply.gitee.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
